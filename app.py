ac917480b19b102f5c8d3f24f285520e88ba0ee6
Tom Creuse <tom.creuse@gmail.com>
2025-02-10 22:00:49 +0100

    modif app

--- app.py
+++ app.py
@@ -12,6 +12,17 @@ from nltk.stem import PorterStemmer, WordNetLemmatizer
 import tensorflow as tf
 import tensorflow_hub as hub
 
+import nltk
+import os
+# Définir un répertoire personnalisé pour les données NLTK
+nltk_data_dir = os.path.join(os.getcwd(), "nltk_data")
+os.makedirs(nltk_data_dir, exist_ok=True)
+nltk.data.path.append(nltk_data_dir)
+
+# Télécharger les ressources NLTK nécessaires
+nltk.download('punkt', download_dir=nltk_data_dir)
+nltk.download('stopwords', download_dir=nltk_data_dir)
+nltk.download('wordnet', download_dir=nltk_data_dir)
 
 app = Flask(__name__)
 
