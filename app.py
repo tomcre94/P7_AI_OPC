482452ec06cedc2d4c2d7dfe2957fcc815b64637
Tom Creuse <tom.creuse@gmail.com>
2025-02-11 21:19:45 +0100

    modif app punkt

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import nltk
 nltk.download('punkt')
 nltk.download('stopwords')
 nltk.download('wordnet')
+nltk.download('punkt_tab')
 
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer, WordNetLemmatizer
